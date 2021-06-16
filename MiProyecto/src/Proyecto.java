eb8d4ad8d31f65ba806def674243476e44a97577
Margarita1209 <82923124+Margarita1209@users.noreply.github.com>
2021-06-16 00:37:51 -0400

    Add files via upload

---
+++ MiProyecto/src/Proyecto.java
@@ -0,0 +1,13 @@
+
+public class Proyecto {
+
+	public static void main(String[] args) {
+		String nombre = "Margarita";
+		System.out.println("Mi nombre es " + nombre);
+		int numero = 30;
+		System.out.println("Tengo " + numero + " años");
+		String ciudad = "Temuco";
+		System.out.println("Mi cuidad es " + ciudad);
+	}
+
+}
