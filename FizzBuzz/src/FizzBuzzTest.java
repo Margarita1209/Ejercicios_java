95cd36608e7d606fc4017cfa68cceaed6b303710
Margarita1209 <82923124+Margarita1209@users.noreply.github.com>
2021-06-17 23:29:33 -0400

    Add files via upload

---
+++ FizzBuzz/src/FizzBuzzTest.java
@@ -0,0 +1,15 @@
+
+public class FizzBuzzTest {
+
+	public static void main(String[] args) {
+		
+		FizzBuzz a = new FizzBuzz();
+		
+		System.out.println(a.fizzBuzz(15));
+		System.out.println(a.fizzBuzz(3));
+		System.out.println(a.fizzBuzz(5));
+		System.out.println(a.fizzBuzz(19));
+
+	}
+
+}
